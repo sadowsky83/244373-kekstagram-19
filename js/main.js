81d4e63a90fab3d6195b232cc00e59642f111e4c
Anatoly Sadowsky <sadowsky83@gmail.com>
2020-02-01 11:56:27 +0700

    исправление ошибок задания 6

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var messages = ['Всё отлично!', 'В целом всё неплохо. 
 var names = ['Валентин', 'Джек Шепард', 'Азура', 'Кабан', 'nagibator777', 'X0P0B0D', 'sveta98', 'KotVKedax', 'Ромашка'];
 
 // переменные элементов карточки
-var cardArray = [];
+var cardsArray = [];
 var picture = document.querySelector('#picture').content.querySelector('.picture');
 var pictures = document.querySelector('.pictures');
 
@@ -22,22 +22,32 @@ function getRandomElement(arrName) {
   return arrName[(Math.floor(arrName.length * (Math.random())))];
 }
 
-// создание объекта для массива карточек
+// создание массива комментариев
+function commentArrayGeneration(quantity) {
+  var commentsArray = [];
+  for (var i = 1; i <= quantity; i++) {
+    var comment = {
+      avatar: 'img/avatar-' + getRandomInRange(1, 6) + '.svg',
+      message: getRandomElement(messages),
+      name: getRandomElement(names)
+    };
+    commentsArray.push(comment);
+  }
+  return commentsArray;
+}
+
+// создание массива карточек
 function cardArrayGeneration(quantity) {
   for (var i = 1; i <= quantity; i++) {
     var imgObj = {
       url: 'photos/' + i + '.jpg',
       description: 'Описание фотографии',
       likes: getRandomInRange(15, 200),
-      comments: {
-        avatar: 'img/avatar-' + getRandomInRange(1, 6) + '.svg',
-        message: getRandomInRange(1, 35),
-        name: getRandomElement(names)
-      }
+      comments: commentArrayGeneration(getRandomInRange(1, 10))
     };
-    cardArray.push(imgObj);
+    cardsArray.push(imgObj);
   }
-  return cardArray;
+  return cardsArray;
 }
 
 cardArrayGeneration(25);
@@ -45,9 +55,8 @@ cardArrayGeneration(25);
 // отрисовка карточки
 function renderCard(card) {
   var cardElement = picture.cloneNode(true);
-
   cardElement.querySelector('.picture__img').src = card.url;
-  cardElement.querySelector('.picture__comments').textContent = card.comments.message;
+  cardElement.querySelector('.picture__comments').textContent = card.comments.length;
   cardElement.querySelector('.picture__likes').textContent = card.likes;
   return cardElement;
 }
@@ -55,7 +64,7 @@ function renderCard(card) {
 // создание фрагмента
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < cardArray.length; i++) {
-  fragment.appendChild(renderCard(cardArray[i]));
+for (var i = 0; i < cardsArray.length; i++) {
+  fragment.appendChild(renderCard(cardsArray[i]));
   pictures.appendChild(fragment);
 }
