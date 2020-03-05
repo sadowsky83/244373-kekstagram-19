e548c29cd324c471f5d26f97bb2b9a559100a226
Anatoly Sadowsky <sadowsky83@gmail.com>
2020-03-05 23:50:48 +0700

    Добавляет ползунок

--- js/form.js
+++ js/form.js
@@ -104,5 +104,4 @@
   }
 
   imgUploadEffects.addEventListener('change', changeEffect);
-
 })();
--- js/slider.js
+++ js/slider.js
@@ -2,43 +2,43 @@
 
 (function () {
 
-  // var imgUploadEffectLevel = document.querySelector('.img-upload__effect-level');
-  // var effectLevelValue = document.querySelector('.effect-level__value');
-  // var effectLevelLine = document.querySelector('.effect-level__line');
   var effectLevelPin = document.querySelector('.effect-level__pin');
-  // var effectLevelDepth = document.querySelector('.effect-level__depth');
+  var effectLevelDepth = document.querySelector('.effect-level__depth');
+
+  function makeValueOfFilter(value) {
+    effectLevelPin.style.left = value + 'px';
+    effectLevelDepth.style.width = value + 'px';
+  }
 
   effectLevelPin.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
+    var startCoords = evt.clientX;
 
-    var onMouseMove = function (moveEvt) {
+    function onMouseMove(moveEvt) {
       moveEvt.preventDefault();
 
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
+      var shift = startCoords - moveEvt.clientX;
+      var position = effectLevelPin.offsetLeft - shift;
+      startCoords = moveEvt.clientX;
 
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
+      if (position <= 0) {
+        position = 0;
+      }
 
-      effectLevelPin.style.top = (effectLevelPin.offsetTop - shift.y) + 'px';
-      effectLevelPin.style.left = (effectLevelPin.offsetLeft - shift.x) + 'px';
-    };
+      if (position > 450) {
+        position = 450;
+      }
 
-    var onMouseUp = function (upEvt) {
+      makeValueOfFilter(position);
+    }
+
+    function onMouseUp(upEvt) {
       upEvt.preventDefault();
 
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
-    };
+    }
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
